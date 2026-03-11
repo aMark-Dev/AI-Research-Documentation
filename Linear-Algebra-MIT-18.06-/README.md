2410dc8e20900cc11a48782641f5639afe194d25
aMark-Dev <marcos.dev2025@gmail.com>
2026-03-11 01:18:07 -0300

    fix: include subfolder contents by removing embedded git

---
+++ Linear-Algebra-MIT-18.06-/README.md
@@ -0,0 +1,2 @@
+# Linear-Algebra (MIT-18.06)
+This is a project aimed at studying linear algebra, with a focus on AI Research.
---
+++ Linear-Algebra-MIT-18.06-/The_geometry_of_linear_equations/function.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <stdio.h>
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    
+
+    return 0;
+}
